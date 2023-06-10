33001eb4f1386627ac531c2a7185990975f31710
kkobena <badoukobena@gmail.com>
2023-06-10 20:49:59 +0200

    etat de control achat annuel

--- src/main/java/rest/service/impl/EtatControlBonServiceImpl.java
+++ src/main/java/rest/service/impl/EtatControlBonServiceImpl.java
@@ -1,5 +1,6 @@
 package rest.service.impl;
 
+import commonTasks.dto.TvaDTO;
 import dal.TBonLivraison;
 import dal.TBonLivraisonDetail;
 import dal.TBonLivraisonDetail_;
@@ -7,15 +8,24 @@ import dal.TBonLivraison_;
 import dal.TFamille_;
 import dal.TGrossiste_;
 import dal.TOrder_;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.Tuple;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -25,6 +35,8 @@ import javax.persistence.criteria.Root;
 import org.apache.commons.lang3.StringUtils;
 import org.json.JSONObject;
 import rest.service.EtatControlBonService;
+import rest.service.dto.EtatControlAnnuelDTO;
+import rest.service.dto.EtatControlAnnuelWrapperDTO;
 import rest.service.dto.EtatControlBon;
 import rest.service.dto.builder.EtatControlBonBuilder;
 import util.FunctionUtils;
@@ -35,16 +47,22 @@ import util.FunctionUtils;
  */
 @Stateless
 public class EtatControlBonServiceImpl implements EtatControlBonService {
-
+    
+    private static final Logger LOG = Logger.getLogger(EtatControlBonServiceImpl.class.getName());
+    private static final String BON_ANNUEL_SQL = " SELECT {groupByLibelle} AS groupByLibelle, SUM(b.`int_MHT`) AS montantHtaxe,SUM(b.`int_TVA`) montantTaxe, sum(b.`int_HTTC`) montantTtc,COUNT(b.`lg_BON_LIVRAISON_ID`) AS nbreBon, SUM(item.itemPrixVente) AS montantVenteTtc  FROM  t_bon_livraison b,t_order o,t_grossiste g,groupefournisseur gp,(SELECT d.`lg_BON_LIVRAISON_ID` AS bonId,   SUM(d.`int_PRIX_VENTE`* d.`int_QTE_RECUE`) AS itemPrixVente FROM t_bon_livraison_detail d GROUP BY bonId) AS item"
+            + "  WHERE item.bonId =b.`lg_BON_LIVRAISON_ID` AND  DATE(b.`dt_DATE_LIVRAISON`) BETWEEN  ?1 AND ?2 AND b.`str_STATUT`='is_Closed' AND b.`lg_ORDER_ID` =o.`lg_ORDER_ID` AND o.`lg_GROSSISTE_ID` =g.`lg_GROSSISTE_ID` AND g.`groupeId`=gp.id  {grossisteIdClose} {groupeIdClose}  GROUP BY groupByLibelle ";
+    
+    private static final String BON_ANNUEL_SQL_GROUP = " SELECT  SUM(b.`int_MHT`) AS montantHtaxe,SUM(b.`int_TVA`) montantTaxe, sum(b.`int_HTTC`) montantTtc,COUNT(b.`lg_BON_LIVRAISON_ID`) AS nbreBon, SUM(item.itemPrixVente) AS montantVenteTtc  FROM  t_bon_livraison b,t_order o,t_grossiste g,groupefournisseur gp,(SELECT d.`lg_BON_LIVRAISON_ID` AS bonId,   SUM(d.`int_PRIX_VENTE`* d.`int_QTE_RECUE`) AS itemPrixVente FROM t_bon_livraison_detail d GROUP BY bonId) AS item"
+            + "  WHERE item.bonId =b.`lg_BON_LIVRAISON_ID` AND  DATE(b.`dt_DATE_LIVRAISON`) BETWEEN  ?1 AND ?2 AND b.`str_STATUT`='is_Closed' AND b.`lg_ORDER_ID` =o.`lg_ORDER_ID` AND o.`lg_GROSSISTE_ID` =g.`lg_GROSSISTE_ID` AND g.`groupeId`=gp.id  {grossisteIdClose} {groupeIdClose}  ";
+    
     @PersistenceContext(unitName = "JTA_UNIT")
     private EntityManager em;
-
-    private boolean returnFullBLLAuthority;
+    
     private static final String DELETE = "delete";
-
+    
     @Override
-    public List<EtatControlBon> list(String search, String dtStart, String dtEnd, String grossisteId, int start, int limit, boolean all) {
-
+    public List<EtatControlBon> list(boolean fullAuth, String search, String dtStart, String dtEnd, String grossisteId, int start, int limit, boolean all) {
+        
         CriteriaBuilder cb = em.getCriteriaBuilder();
         CriteriaQuery<TBonLivraison> cq = cb.createQuery(TBonLivraison.class);
         Root<TBonLivraison> root = cq.from(TBonLivraison.class);
@@ -56,13 +74,13 @@ public class EtatControlBonServiceImpl implements EtatControlBonService {
         if (!all) {
             q.setFirstResult(start);
             q.setMaxResults(limit);
-
+            
         }
         return q.getResultList().stream().map(e -> EtatControlBonBuilder.build(e))
-                .peek(e1 -> e1.setReturnFullBl((!DELETE.equals(e1.getStrSTATUT()) && e1.getIntHTTC() > 0) && this.returnFullBLLAuthority))
+                .peek(e1 -> e1.setReturnFullBl((!DELETE.equals(e1.getStrSTATUT()) && e1.getIntHTTC() > 0) && fullAuth))
                 .collect(Collectors.toList());
     }
-
+    
     public long count(String search, String dtStart, String dtEnd, String grossisteId) {
         CriteriaBuilder cb = em.getCriteriaBuilder();
         CriteriaQuery<Long> cq = cb.createQuery(Long.class);
@@ -73,13 +91,13 @@ public class EtatControlBonServiceImpl implements EtatControlBonService {
         cq.where(cb.and(predicates.toArray(new Predicate[0])));
         TypedQuery<Long> q = em.createQuery(cq);
         return Objects.isNull(q.getSingleResult()) ? 0 : q.getSingleResult();
-
+        
     }
-
+    
     private List<Predicate> listPredicates(CriteriaBuilder cb, Root<TBonLivraison> root,
             LocalDate dtStart, LocalDate dtEnd, String grossisteId, String search) {
         List<Predicate> predicates = new ArrayList<>();
-
+        
         Predicate btw = cb.between(cb.function("DATE", Date.class, root.get(TBonLivraison_.dtDATELIVRAISON)),
                 java.sql.Date.valueOf(dtStart), java.sql.Date.valueOf(dtEnd));
         predicates.add(btw);
@@ -97,16 +115,125 @@ public class EtatControlBonServiceImpl implements EtatControlBonService {
         }
         return predicates;
     }
-
+    
     @Override
-    public JSONObject list(String search, String dtStart, String dtEnd, String grossisteId, int start, int limit) {
+    public JSONObject list(boolean fullAuth, String search, String dtStart, String dtEnd, String grossisteId, int start, int limit) {
         long count = count(search, dtStart, dtEnd, grossisteId);
-        return FunctionUtils.returnData(list(search, dtStart, dtEnd, grossisteId, start, limit, false), count);
+        return FunctionUtils.returnData(list(fullAuth, search, dtStart, dtEnd, grossisteId, start, limit, false), count);
     }
-
+    
     @Override
-    public void hasReturnFullBLLAuthority(boolean b) {
-        this.returnFullBLLAuthority = b;
+    public EtatControlAnnuelWrapperDTO listBonAnnuel(String groupBy, String dtStart, String dtEnd, String grossisteId, Integer groupeId) {
+        
+        List<EtatControlAnnuelDTO> list = listBonsAnnuel(groupBy, dtStart, dtEnd, grossisteId, groupeId);
+        EtatControlAnnuelWrapperDTO annuelSummary = bonsAnnuelGroup(dtStart, dtEnd, grossisteId, groupeId);
+        annuelSummary.setEtatControlAnnuels(list.stream().peek(el
+                -> el.setPourcentage(computePercent(el.getMontantTtc(), annuelSummary.getSummary().getTotalTtc())))
+                .sorted(Comparator.comparing(EtatControlAnnuelDTO::getGroupByLibelle))
+                .collect(Collectors.toList()));
+        
+        return annuelSummary;
+        
     }
-
+    
+    private float computePercent(long montantTTC, long totalTTC) {
+        return BigDecimal.valueOf((montantTTC * 100) / Double.valueOf(totalTTC)).setScale(2, RoundingMode.HALF_UP).floatValue();
+    }
+    
+    private EtatControlAnnuelDTO buildEtatControlAnnuels(Tuple tuple) {
+        long montantTtc = tuple.get("montantTtc", BigDecimal.class).longValue();
+        long montantVenteTtc = tuple.get("montantVenteTtc", BigDecimal.class).longValue();
+        long montantTaxe = tuple.get("montantTaxe", BigDecimal.class).longValue();
+        long marge = montantVenteTtc - montantTtc;
+        return EtatControlAnnuelDTO.builder()
+                .montantMarge(marge)
+                .montantHtaxe(tuple.get("montantHtaxe", BigDecimal.class).longValue())
+                .nbreBon(tuple.get("nbreBon", BigInteger.class).intValue())
+                .montantTtc(montantTtc)
+                .montantTaxe(montantTaxe)
+                .groupByLibelle(tuple.get("groupByLibelle", String.class))
+                .montantVenteTtc(montantVenteTtc)
+                .build();
+    }
+    
+    private List<EtatControlAnnuelDTO> listBonsAnnuel(String groupBy, String dtStart, String dtEnd, String grossisteId, Integer groupeId) {
+        String sql = BON_ANNUEL_SQL;
+        sql = buildSqlQuery(grossisteId, groupeId, sql);
+        
+        if ("GROUP".equals(groupBy)) {
+            sql = sql.replace("{groupByLibelle}", " gp.libelle ");
+        } else {
+            sql = sql.replace("{groupByLibelle}", " g.`str_LIBELLE` ");
+        }
+        
+        LOG.log(Level.INFO, "sql--- listBonAnnuel  {0}", sql);
+        try {
+            Query query = em.createNativeQuery(sql, Tuple.class)
+                    .setParameter(1, java.sql.Date.valueOf(dtStart))
+                    .setParameter(2, java.sql.Date.valueOf(dtEnd));
+            List<Tuple> list = query.getResultList();
+            return list.stream().map(this::buildEtatControlAnnuels).collect(Collectors.toList());
+            
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE, null, e);
+            return Collections.emptyList();
+        }
+    }
+    
+    private EtatControlAnnuelWrapperDTO bonsAnnuelGroup(String dtStart, String dtEnd, String grossisteId, Integer groupeId) {
+        String sql = BON_ANNUEL_SQL_GROUP;
+        sql = buildSqlQuery(grossisteId, groupeId, sql);
+        
+        LOG.log(Level.INFO, "sql--- bonsAnnuelGroup  {0}", sql);
+        try {
+            Query query = em.createNativeQuery(sql, Tuple.class)
+                    .setParameter(1, java.sql.Date.valueOf(dtStart))
+                    .setParameter(2, java.sql.Date.valueOf(dtEnd));
+            Tuple tuple = (Tuple) query.getSingleResult();
+            return buildEtatControlAnnuelSummaryDTO(tuple);
+            
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE, null, e);
+            return EtatControlAnnuelWrapperDTO.builder().build();
+        }
+    }
+    
+    private EtatControlAnnuelWrapperDTO buildEtatControlAnnuelSummaryDTO(Tuple tuple) {
+        long montantTtc = tuple.get("montantTtc", BigDecimal.class).longValue();
+        long montantVenteTtc = tuple.get("montantVenteTtc", BigDecimal.class).longValue();
+        long montantTaxe = tuple.get("montantTaxe", BigDecimal.class).longValue();
+        long marge = montantVenteTtc - montantTtc;
+        return EtatControlAnnuelWrapperDTO.builder()
+                .summary(EtatControlAnnuelWrapperDTO.EtatControlAnnuelSummary.builder()
+                        .totalMarge(marge)
+                        .totaltHtaxe(tuple.get("montantHtaxe", BigDecimal.class).longValue())
+                        .totalNbreBon(tuple.get("nbreBon", BigInteger.class).intValue())
+                        .totalTtc(montantTtc)
+                        .totalTaxe(montantTaxe)
+                        .totalVenteTtc(montantVenteTtc)
+                        .build())
+                .build();
+    }
+    
+    @Override
+    public JSONObject listBonAnnuelView(String groupBy, String dtStart, String dtEnd, String grossisteId, Integer groupeId) {
+        EtatControlAnnuelWrapperDTO annuelSummary = listBonAnnuel(groupBy, dtStart, dtEnd, grossisteId, groupeId);
+        List<EtatControlAnnuelDTO> annuels = annuelSummary.getEtatControlAnnuels();
+        return FunctionUtils.returnData(annuels, annuels.size(), annuelSummary.getSummary());
+    }
+    
+    private String buildSqlQuery(String grossisteId, Integer groupeId, String sql) {
+        if (StringUtils.isNotEmpty(grossisteId)) {
+            sql = sql.replace("{grossisteIdClose}", String.format("  AND g.`lg_GROSSISTE_ID`= %s ", grossisteId));
+        } else {
+            sql = sql.replace("{grossisteIdClose}", " ");
+        }
+        if (Objects.nonNull(groupeId) && groupeId != 0) {
+            sql = sql.replace("{groupeIdClose}", String.format("  AND gp.id= %d ", groupeId));
+        } else {
+            sql = sql.replace("{groupeIdClose}", " ");
+        }
+        
+        return sql;
+    }
 }
