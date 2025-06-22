d69aa2703ee38f401e104b1405833b287b175ea0
FRANCK HERMANN N'ZI <hermannzi@outlook.fr>
2025-06-22 21:41:50 +0100

    fusion dev apis all in one file skippath

--- src/main/java/filter/AuthenticationFilter.java
+++ src/main/java/filter/AuthenticationFilter.java
@@ -55,41 +55,14 @@ public class AuthenticationFilter implements ContainerRequestFilter {
     }
 
     private boolean skipPath(String path) {
-    Set<String> paths = Set.of(
-        "v1/user/auth", 
-        "v1/user/logout", 
-        "v1/ws/", 
-        "v1/valorisation", 
-        "v1/valorisation/all", 
-        "v1/ca-comptant",
-        "v1/ca-credit", 
-        "v1/reglements", 
-        "v1/factures", 
-        "v1/fournisseurs", 
-        "v1/achats-fournisseurs", 
-        "v1/stock",
-        "v1/tierspayants", 
-        "v1/avoirs-fournisseurs", 
-        "v1/whareouse-vno", 
-        "v1/whareouse-maxmin", 
-        "v1/ca-all",
-        "v1/checkproduit", 
-        "v1/ws/ca-achats-ventes", 
-        "v1/ws/inventaires", 
-        "v1/ws/inventaires/rayons",
-        "v1/ws/inventaires/details", 
-        "v1/balance/etat-annuel", 
-        "v1/etat-control-bon/etat-annuel",
-        "v1/balance/balanceventecaisse", 
-        "v1/recap/dashboardmob",
-        "v1/recap/creditsmob",
-        "v1/recap/credits/totauxmob",
-        "v3/tvamobile",
-        "v1/produit/stats/vente-annuellep",
-        "v1/evaluation-vente/produit",
-        "v1/info",
-        "v1/officine"
-    );
-    return paths.contains(path);
-}
+        Set<String> paths = Set.of("v1/user/auth", "v1/user/logout", "v1/ws/", "v1/valorisation", "v1/valorisation/all",
+                "v1/ca-comptant", "v1/ca-credit", "v1/reglements", "v1/factures", "v1/fournisseurs",
+                "v1/achats-fournisseurs", "v1/stock", "v1/tierspayants", "v1/avoirs-fournisseurs", "v1/whareouse-vno",
+                "v1/whareouse-maxmin", "v1/ca-all", "v1/checkproduit", "v1/ws/ca-achats-ventes", "v1/ws/inventaires",
+                "v1/ws/inventaires/rayons", "v1/ws/inventaires/details", "v1/balance/etat-annuel",
+                "v1/etat-control-bon/etat-annuel", "v1/balance/balanceventecaisse", "v1/recap/dashboardmob",
+                "v1/recap/creditsmob", "v1/recap/credits/totauxmob", "v3/tvamobile", "v1/produit/stats/vente-annuellep",
+                "v1/evaluation-vente/produit", "v1/info", "v1/officine");
+        return paths.contains(path);
+    }
 }
