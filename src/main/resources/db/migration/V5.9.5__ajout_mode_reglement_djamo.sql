b7ccf9ff4c3e0b2639bdfb57e362a44a7866f53a
FRANCK HERMANN N'ZI <hermannzi@outlook.fr>
2026-02-10 10:42:30 +0000

    ajout djamo script celpaid tresor pay et djamo

--- src/main/resources/db/migration/V5.9.5__ajout_mode_reglement_djamo.sql
+++ src/main/resources/db/migration/V5.9.5__ajout_mode_reglement_djamo.sql
@@ -1,3 +1,8 @@
-INSERT INTO `t_type_reglement` (`lg_TYPE_REGLEMENT_ID`, `str_NAME`, `str_DESCRIPTION`, `str_STATUT`, `dt_CREATED`) VALUES ('19', 'DJAMO', 'DJAMO', 'enable', '2016-04-29 14:38:58');
-INSERT INTO `t_mode_reglement` (`lg_MODE_REGLEMENT_ID`, `str_NAME`, `str_DESCRIPTION`, `lg_TYPE_REGLEMENT_ID`, `dt_CREATED`, `dt_UPDATED`, `str_STATUT`) VALUES ('19', 'DJAMO', 'DJAMO', '19', '2016-04-29 14:46:25', '2016-04-29 14:46:25', 'enable');
+INSERT INTO `t_type_reglement` (`lg_TYPE_REGLEMENT_ID`, `str_NAME`, `str_DESCRIPTION`, `str_STATUT`, `dt_CREATED`) VALUES ('19', 'DJAMO', 'DJAMO', 'enable', '2026-02-10 10:38:58');
+INSERT INTO `t_mode_reglement` (`lg_MODE_REGLEMENT_ID`, `str_NAME`, `str_DESCRIPTION`, `lg_TYPE_REGLEMENT_ID`, `dt_CREATED`, `dt_UPDATED`, `str_STATUT`) VALUES ('19', 'DJAMO', 'DJAMO', '19', '2026-02-10 10:38:58', '2026-02-10 10:38:58', 'enable');
 
+INSERT INTO `t_type_reglement` (`lg_TYPE_REGLEMENT_ID`, `str_NAME`, `str_DESCRIPTION`, `str_STATUT`, `dt_CREATED`) VALUES ('70', 'CELPAID', 'CELPAID', 'disable', '2016-04-29 14:38:58');
+INSERT INTO `t_mode_reglement` (`lg_MODE_REGLEMENT_ID`, `str_NAME`, `str_DESCRIPTION`, `lg_TYPE_REGLEMENT_ID`, `dt_CREATED`, `dt_UPDATED`, `str_STATUT`) VALUES ('70', 'CELPAID', 'CELPAID', '70', '2026-02-10 10:39:58', '2026-02-10 10:39:58', 'disable');
+
+INSERT INTO `t_type_reglement` (`lg_TYPE_REGLEMENT_ID`, `str_NAME`, `str_DESCRIPTION`, `str_STATUT`, `dt_CREATED`) VALUES ('80', 'TRESORPAY', 'TRESORPAY', 'disable', '2016-04-29 14:38:58');
+INSERT INTO `t_mode_reglement` (`lg_MODE_REGLEMENT_ID`, `str_NAME`, `str_DESCRIPTION`, `lg_TYPE_REGLEMENT_ID`, `dt_CREATED`, `dt_UPDATED`, `str_STATUT`) VALUES ('80', 'TRESORPAY', 'TRESORPAY', '80', '2026-02-10 10:40:58', '2026-02-10 10:40:58', 'disable');
