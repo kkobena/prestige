0bb13b056d59ac38299f6a723d6503c1f672adea
FRANCK HERMANN N'ZI <hermannzi@outlook.fr>
2026-02-10 10:31:46 +0000

    ajout djamo script

---
+++ src/main/resources/db/migration/V5.9.5__ajout_mode_reglement_djamo.sql
@@ -0,0 +1,3 @@
+INSERT INTO `t_type_reglement` (`lg_TYPE_REGLEMENT_ID`, `str_NAME`, `str_DESCRIPTION`, `str_STATUT`, `dt_CREATED`) VALUES ('19', 'DJAMO', 'DJAMO', 'enable', '2016-04-29 14:38:58');
+INSERT INTO `t_mode_reglement` (`lg_MODE_REGLEMENT_ID`, `str_NAME`, `str_DESCRIPTION`, `lg_TYPE_REGLEMENT_ID`, `dt_CREATED`, `dt_UPDATED`, `str_STATUT`) VALUES ('19', 'DJAMO', 'DJAMO', '19', '2016-04-29 14:46:25', '2016-04-29 14:46:25', 'enable');
+
